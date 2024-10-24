732e9d7990c736976e64ab5183481e30c70c2a91
11rpl2-gunawan <guamawanbrj123@gmail.com>
2024-10-24 23:32:36 +0700

    guanawan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +13,16 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+    // Route untuk menampilkan profile
+    Route::get('/show', [ProfileController::class, 'show'])->name('show');
+
+    // Route untuk mengedit profile (form edit)
+    Route::get('/edit', [ProfileController::class, 'edit'])->name('edit');
+
+    // Route untuk update profile (submit form edit)
+    Route::PATCH('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+});
 
 require __DIR__.'/auth.php';
